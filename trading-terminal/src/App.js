c6591b39d5ba8756153e004f5a9279109f53c714
Shubham <3435671+Shubhamgoyal26@users.noreply.github.com>
2023-04-16 16:59:52 +0530

    Change title

--- trading-terminal/src/App.js
+++ trading-terminal/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="title">Terminal</h1>
+      <h1 className="title">Trading Tool</h1>
     </div>
   );
 }
