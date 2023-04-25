a44b16a8022932f9a5450440f427a9c09921c67a
Shivam Jain <jainshivam1103@gmail.com>
2023-04-25 17:06:12 +0530

    login ui

--- trading-terminal/src/pages/Login.jsx
+++ trading-terminal/src/pages/Login.jsx
@@ -3,69 +3,35 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <div className="container p-6">
-        <div className="row">
-          <div className="cl-md-4 offset-md-4">
-            <div className="card p-6" style={{ height: "60vh" }}>
-              <div className="card-body">
-                <h2
-                  className="text-center"
-                  style={{ fontSize: "x-large", fontWeight: "bolder" }}
-                >
-                  Login Page
-                </h2>
-                <form action="login" method="post">
-                  <div className="form-group">
-                    <label
-                      for="exampleInputEmail1"
-                      style={{ fontWeight: "bold", paddingTop: "5%" }}
-                    >
-                      Email address
-                    </label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="exampleInputEmail1"
-                      aria-describedby="emailHelp"
-                      placeholder="Enter email"
-                      required="required"
-                      name="email"
-                    />
-                    <small id="emailHelp" className="form-text text-muted">
-                      We'll never share your email with anyone else.
-                    </small>
-                  </div>
-
-                  <div className="form-group">
-                    <label
-                      for="exampleInputPassword1"
-                      style={{ fontWeight: "bold" }}
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="exampleInputPassword1"
-                      placeholder="Password"
-                      required="required"
-                      name="password"
-                    />
-                  </div>
-
-                  <div className="text-center mt-2">
-                    <button type="submit" className="button is-primary">
-                      Login
-                    </button>
-                    <div className="text-center mt-2">
-                      <a href="register.jsp">Create Account</a>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="container ">
+      <div class="field">
+  <p class="control has-icons-left has-icons-right">
+  <label class="label">Email</label>
+    <input class="input" type="email" placeholder="Email"/>
+    <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control has-icons-left">
+  <label class="label">Password</label>
+    <input class="input" type="password" placeholder="Password"/>
+    <span class="icon is-small is-left">
+      <i class="fas fa-lock"></i>
+    </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control">
+    <button class="button is-success">
+      Login
+    </button>
+  </p>
+</div>
       </div>
     </div>
   );
