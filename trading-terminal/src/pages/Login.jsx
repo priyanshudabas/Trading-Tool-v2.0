5080b20f22ab2dbd952678731da979625dfff5a2
Shubham <53435671+Shubhamgoyal26@users.noreply.github.com>
2023-04-25 17:29:39 +0530

    Style login form

--- trading-terminal/src/pages/Login.jsx
+++ trading-terminal/src/pages/Login.jsx
@@ -1,37 +1,37 @@
-import React from "react";
+import React from 'react';
 
 const Login = () => {
   return (
     <div>
-      <div className="container ">
-      <div class="field">
-  <p class="control has-icons-left has-icons-right">
-  <label class="label">Email</label>
-    <input class="input" type="email" placeholder="Email"/>
-    <span class="icon is-small is-left">
-      <i class="fas fa-envelope"></i>
-    </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
-    </span>
-  </p>
-</div>
-<div class="field">
-  <p class="control has-icons-left">
-  <label class="label">Password</label>
-    <input class="input" type="password" placeholder="Password"/>
-    <span class="icon is-small is-left">
-      <i class="fas fa-lock"></i>
-    </span>
-  </p>
-</div>
-<div class="field">
-  <p class="control">
-    <button class="button is-success">
-      Login
-    </button>
-  </p>
-</div>
+      <div className="container center-container mt-6">
+        <div className="card card1 p-6">
+          <div class="field">
+            <p class="control has-icons-left has-icons-right">
+              <label class="label">Email</label>
+              <input class="input" type="email" placeholder="Email" />
+              <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+              </span>
+              <span class="icon is-small is-right">
+                <i class="fas fa-check"></i>
+              </span>
+            </p>
+          </div>
+          <div class="field">
+            <p class="control has-icons-left">
+              <label class="label">Password</label>
+              <input class="input" type="password" placeholder="Password" />
+              <span class="icon is-small is-left">
+                <i class="fas fa-lock"></i>
+              </span>
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <button class="button is-success mt-2">Login</button>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
