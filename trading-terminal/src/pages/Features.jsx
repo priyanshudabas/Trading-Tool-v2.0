7cd6fba9e57196396b36b9ff22f3eec5533045d2
priyanshudabas <96956256+priyanshudabas@users.noreply.github.com>
2023-04-25 17:13:41 +0530

    UI Changes

--- trading-terminal/src/pages/Features.jsx
+++ trading-terminal/src/pages/Features.jsx
@@ -6,59 +6,10 @@ import strategyAlerts from "../assets/strategyAlerts.png";
 const Features = () => {
   return (
     <div>
-      {/* <div className="container m-6">
-        <h1 className="title is-1">Platform Features </h1>
-      </div>
-     <div className="card mx-6 p-6" style={{height:"70vh", width:"95%"}}>
-     <div className="container"> </div>
-      <div className="columns">
-        <div className="column is-1"></div>
-        <div className="column">
-          <p className="content is-large">
-            <b>Manual Trading</b>
-          </p>
-          <p className="content is-medium">
-            Manually entering trades as per personal techincal analysis and
-            executing them.
-          </p>
-          <figure>
-            <img src={manualTrading} className="is-rounded" alt="manual trading"/>
-          </figure>
-        </div>
-        <div className="column is -1"></div>
-        <div className="column is-centered">
-          <p className="content is-large">
-            <b>Automated Trading</b>
-          </p>
-          <p className="content is-medium">
-            Sending trades automatically to the exchange as the strategy
-            generates signals.
-          </p>
-          <figure>
-            <img src={automatedTrading} alt="automatecd trading"/>
-          </figure>
-        </div>
-        <div className="column is -1"></div>
-        <div className="column is-centered">
-          <p className="content is-large">
-            <b>Strategy Alerts</b>
-          </p>
-          <p className="content is-medium">
-            Alert the user as and when the strategy generates any form of
-            signal.
-          </p>
-          <figure>
-            <img src={strategyAlerts} alt="strategy alerts"/>
-          </figure>
-        </div>
-        <div className="column is -1"></div>
-      </div>
-
-     </div> */}
       <div className="container m-6">
         <h1 className="title is-1">Platform Features </h1>
       </div>
-      <div className="card m-6 p-6" style={{ display: "flex"}}>
+      <div className="card m-6 p-6" style={{ display: "flex" }}>
         <div className="card m-3 p-6">
           <p className="content is-large">
             <b>Manual Trading</b>
@@ -68,14 +19,14 @@ const Features = () => {
             executing them.
           </p>
           <div class="card-image">
-          <figure>
-            <img
-              src={manualTrading}
-              className="is-rounded"
-              alt="manual trading"
-              style={{height: "300px", width: "350px"}}
-            />
-          </figure>
+            <figure>
+              <img
+                src={manualTrading}
+                className="is-rounded"
+                alt="manual trading"
+                style={{ height: "300px", width: "350px" }}
+              />
+            </figure>
           </div>
         </div>
         <div className="card m-3 p-6">
@@ -87,10 +38,13 @@ const Features = () => {
             generates signals.
           </p>
           <div class="card-image">
-          <figure>
-            <img src={automatedTrading} alt="automated trading"               style={{height: "300px", width: "350px"}}
-/>
-          </figure>
+            <figure>
+              <img
+                src={automatedTrading}
+                alt="automated trading"
+                style={{ height: "300px", width: "350px" }}
+              />
+            </figure>
           </div>
         </div>
         <div className="card m-3 p-6">
@@ -102,10 +56,13 @@ const Features = () => {
             signal.
           </p>
           <div class="card-image">
-          <figure>
-            <img src={strategyAlerts} alt="strategy alerts"               style={{height: "300px", width: "300px"}}
-/>
-          </figure>
+            <figure>
+              <img
+                src={strategyAlerts}
+                alt="strategy alerts"
+                style={{ height: "300px", width: "300px" }}
+              />
+            </figure>
           </div>
         </div>
       </div>
